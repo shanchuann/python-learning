f2c888d9677e01e9f2a3eb2df8100afd05c490b9
Bokun Zhang(张铂坤) <1537521192@qq.com>
2024-08-21 09:39:52 +0800

    Resolved out of index error

--- unit 10 Files and IO operations/code 10.6 csv.py
+++ unit 10 Files and IO operations/code 10.6 csv.py
@@ -1,30 +1,26 @@
-import csv,random
+import csv, random
 from my_package import my_tools
 lista = []
 def random_info(n = 100):
-    subjects = ['Math', 'Science', 'History']
+    subjects = ['python', 'java', 'c++', 'c#', 'javascript']
     for i in range(n):
-        name = my_tools.random_string(random.randint(3,6))
+        name = my_tools.random_string(random.randint(3, 6))
         subject = random.choice(subjects)
-        score = random.randint(50,100)
+        score = random.randint(50, 100)
         lista.append([name, subject, score])
-
 def average():
-    with open('data.csv', mode = 'r',encoding = 'utf-8') as f:
+    with open('data.csv', mode = 'r', encoding = 'utf-8') as f:
         cf = csv.reader(f)
-        head = next(cf)
-        scores = []
-        print(head)
-        for row in cf:
-            scores.append(int(row[2]))
-        return sum(scores)/len(scores)
-
+        head = next(cf)  # 获取表头
+        score = []
+        for i in cf:
+            score.append(int(i[2]))
+    return sum(score)/len(score)
 def make_datas():
-    with open('data.csv', mode = 'a',encoding = 'utf-8') as f:
+    with open('data.csv', mode = 'a', encoding = 'utf-8',newline = "") as f:
         cf = csv.writer(f)
         random_info()
         cf.writerows(lista)
 
 make_datas()
-result = average()
-print('The average score is:', result)
+print('average is',round(average(), 2))
