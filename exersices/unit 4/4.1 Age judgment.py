f238b7bffebe17458230a35f0d70bbe78c50de1e
山川不念旧 <1537521192@qq.com>
2026-06-21 17:50:29 +0800

    fix：修复'int' object has no attribute 'isdigit'

--- exersices/unit 4/4.1 Age judgment.py
+++ exersices/unit 4/4.1 Age judgment.py
@@ -1,7 +1,7 @@
 # 年龄判断
-age = int(input("请输入一个人的年龄："))
-if age.isdigit():
-    age = int(age)
+age_str = input("请输入一个人的年龄：")
+if age_str.isdigit():
+    age = int(age_str)
     if age >= 1 and age <= 120:
         print("年龄在1-120之间")
     else:
